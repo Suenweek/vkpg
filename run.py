46870a68355f6b6b20db6d2af81a6506fda41cf3
Suenweek <suenweek@protonmail.com>
2017-11-29 15:18:43 +0300

    Add `webbrowser.open` and cli args for `run.py`

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
 from argparse import ArgumentParser
 from app import app
+import webbrowser
 
 
-HOST = "127.0.0.1"
-PORT = 5000
+LOCALHOST = "127.0.0.1"
 
 
 def main(args):
-    app.run(HOST, PORT)
+    if not args.no_browser:
+        webbrowser.open("%s:%d" % (LOCALHOST, args.port))
+    app.run(LOCALHOST, args.port, debug=args.debug)
 
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
+    arg_parser.add_argument("port", nargs="?", type=int, default=5000)
+    arg_parser.add_argument("-d", "--debug", dest="debug", action="store_true")
+    arg_parser.add_argument("-nb", "--no-browser", dest="no_browser", action="store_true")
     main(arg_parser.parse_args())
