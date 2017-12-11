5321a005ebd45898b5bbc2b6b2eb7ee957480fb4
Suenweek <suenweek@protonmail.com>
2017-12-11 18:09:20 +0300

    Add support for profile, wall and saved photos

--- app/urls.py
+++ app/urls.py
@@ -6,17 +6,30 @@ class VkAlbumUrl(object):
     """
     Represents vk url for a photo album
     """
+    REGEX = re.compile(r"/album(\-{0,1}\d+)_(\d+)")
+
     def __init__(self, url):
-        self.regex = re.compile(r"/album(\-{0,1}\d+)_(\d+)")
-        self.owner_id, self.album_id = self.parse(url)
+        groups = self.parse(url)
+        self.owner_id = self.parse_owner_id(groups[0])
+        self.album_id = self.parse_album_id(groups[1])
 
     def __str__(self):
         return "<VkAlbumUrl(owner_id='%s', album_id='%s')>" % (self.owner_id, self.album_id)
 
     def parse(self, url):
         path = urlparse(url).path
-        search = self.regex.search(path)
-        if search is not None:
-            return search.groups()
+        search_result = VkAlbumUrl.REGEX.search(path)
+        if search_result is not None:
+            return search_result.groups()
         else:
             raise ValueError("Invalid album url")
+
+    def parse_owner_id(self, owner_id):
+        return owner_id
+
+    def parse_album_id(self, album_id):
+        return {
+            "0": "profile",
+            "00": "wall",
+            "000": "saved"
+        }.get(album_id, album_id)
